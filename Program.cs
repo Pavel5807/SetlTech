7625dd315c1009ed54cfc3d8e096c09a7d3bf715
Pavel Zyryanov <5807keam.hi@gmail.com>
2024-05-16 23:31:04 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+Console.WriteLine("Введите путь до файла .jpg");
+
+var path = Console.ReadLine();
+var image = new Image<Bgr, byte>(path);
+
+var classifier = new CascadeClassifier("haarcascade_plate_number.xml");
+var faces = classifier.DetectMultiScale(image);
+
+foreach (var face in faces)
+{
+    image.Draw(face, new Bgr(Color.Red), 2);
+}
+
+Console.WriteLine("Изображение сохранено в \"number.jpg\"");
+image.Save("number.jpg");
